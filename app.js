b31919f5a26120cbc621c2df18394ed97935cc16
bereket <iberekety@gmail.com>
2023-08-03 18:41:04 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// const express = require('express')
+import express from 'express'
+import HelloController from './controllers/hello-controller.js'
+import UsersController from './controllers/users/users-controller.js';
+import TuitsController from './controllers/tuits/tuits-controller.js';
+import cors from 'cors'
+
+
+const app = express()
+app.use(cors()) // enable ALL CORS requests (client requests from other domain)
+app.use(express.json()); // parse JSON from HTTP request body
+
+TuitsController(app);
+// HelloController(app);
+// UsesController(app);
+
+app.listen(4000)
---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,45 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+
+const createTuit = (req, res) => {
+  const newTuit = req.body;                   // retrieve data from HTTP body
+  newTuit._id = (new Date()).getTime() + '';    // add _id field as a time stamp
+  newTuit.likes = 0;                          // initialize likes counter
+  newTuit.liked = false;                    // initialize liked flag      
+  tuits.push(newTuit);                        // append new tuit to tuits array
+  res.json(newTuit);                        // respond with new tuit                
+}                                           // next chapter will store in database instead
+
+
+const findTuits = (req, res) => {
+  res.json(tuits);
+}
+
+const updateTuit = (req, res) => {
+  const tuitsIdToUpdate = req.params.tid;         // get ID of tuit to update from path
+  const updates = req.body;                       // get updates from HTTP body
+  const tuitIndex = tuits.findIndex(              // find index of tuit to update
+    (t) => t._id === tuitsIdToUpdate)             // in the tuits array
+  tuits[tuitIndex] =                              // update the element in tuits array       
+    { ...tuits[tuitIndex], ...updates };          // merging/updating old tuit with updates
+  res.sendStatus(200);                            // respond with success          
+}                                                 // next chapter will remove from database instead
+
+
+const deleteTuit = (req, res) => {
+  const tuitsIdToDelete = req.params.tid;    // retrieve the ID of the tuit we want to remove
+  tuits = tuits.filter((t) =>                // filter out the tuit from the tuits array
+    t._id !== tuitsIdToDelete);
+  res.sendStatus(200);                       // respond with success       
+}
+
+
+const TuitsController = (app) => {
+  app.post('/api/tuits', createTuit);
+  app.get('/api/tuits', findTuits);
+  app.put('/api/tuits/:tid', updateTuit);
+  app.delete('/api/tuits/:tid', deleteTuit);
+}
+
+export default TuitsController;
