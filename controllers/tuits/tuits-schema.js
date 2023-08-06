ae2cc7b50689f321b535ec16036c63c0a69b2465
bereket <iberekety@gmail.com>
2023-08-06 13:21:57 -0400

    Update schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose';         
 
 const schema = mongoose.Schema({
-  tuit: {type: String, required: false},
-  likes: {type: Number, required: false},
-  liked: {type: Boolean, required: false},
+  topic: { type: String, required: false },
+  userName: { type: String, required: true },
+  handle: { type: String, required: true },
+  time: { type: String, required: true },
+  image: { type: String, required: true },
+  title: { type: String, required: false },
+  tuit: { type: String, required: true },
+  liked: { type: Boolean, required: true },
+  likes: { type: Number, required: true },
+  replies: { type: Number, required: true },
+  retuits: { type: Number, required: true },
 },{collections: 'tuits'})
 
 // export default mongoose.model('Tuit', schema);
