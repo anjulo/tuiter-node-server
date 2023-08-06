844969d3d9122127858938e0662f8518d777dca6
bereket <iberekety@gmail.com>
2023-08-06 12:14:17 -0400

    Set-up mongoose

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // const express = require('express')
 import express from 'express'
-import HelloController from './controllers/hello-controller.js'
+// import HelloController from './controllers/hello-controller.js'
 import UsersController from './controllers/users/users-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
 import cors from 'cors'
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 const app = express()
 // app.get('/hello', (req, res) => {res.send('Hello World!')})  // 
@@ -12,7 +14,6 @@ app.use(cors()) // enable ALL CORS requests (client requests from other domain)
 app.use(express.json()); // parse JSON from HTTP request body
 
 TuitsController(app);
-// HelloController(app);
 UsersController(app);
 
 app.listen(4000 || process.env.PORT)
