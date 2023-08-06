d2e7712684de01c2834b6da911e9c4bdd116ae71
bereket <iberekety@gmail.com>
2023-08-06 12:23:12 -0400

    Update CRUD operations to communicate with mongoDB database

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,5 +1,4 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import * as tuitsDAO from './tuits-dao.js';         // import the dao
 
 const TuitsController = (app) => {
   app.post('/api/tuits', createTuit);
@@ -22,38 +21,34 @@ const templateTuit = {
   "replies": 0,
   "retuits": 0,
   "likes": 0,
-  "_id": new Date().getTime() + '',
 }
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
   let newTuit = req.body;                   // retrieve data from HTTP body   
   newTuit = { ...templateTuit, ...newTuit };     // merge with template 
-  tuits.unshift(newTuit);                        // append new tuit to tuits array
-  console.log(newTuit)
-  res.json(newTuit);                        // respond with new tuit                
+  res.json(insertedTuit);                        // respond with new tuit                
 }                                           // next chapter will store in database instead
 
 
-const findTuits = (req, res) => {
+const findTuits = async (req, res) => {
+  // res.json(tuits);
+  const tuits = await tuitsDAO.findTuits()
   res.json(tuits);
 }
 
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
   const tuitsIdToUpdate = req.params.tid;         // get ID of tuit to update from path
   const updates = req.body;                       // get updates from HTTP body
-  const tuitIndex = tuits.findIndex(              // find index of tuit to update
-    (t) => t._id === tuitsIdToUpdate)             // in the tuits array
-  tuits[tuitIndex] =                              // update the element in tuits array       
-    { ...tuits[tuitIndex], ...updates };          // merging/updating old tuit with updates
-  res.sendStatus(200);                            // respond with success          
+  console.log(`Updating tuit ${tuitsIdToUpdate} with ${JSON.stringify(updates)}`)
+  const status = await tuitsDAO.updateTuit(tuitsIdToUpdate, updates); // returns success or failure
+  res.send(status);                                     
 }                                                 // next chapter will remove from database instead
 
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
   const tuitsIdToDelete = req.params.tid;    // retrieve the ID of the tuit we want to remove
-  tuits = tuits.filter((t) =>                // filter out the tuit from the tuits array
-    t._id !== tuitsIdToDelete);
-  res.sendStatus(200);                       // respond with success       
+  const status = await tuitsDAO.deleteTuit(tuitsIdToDelete);
+  res.send(status);                       // respond with success       
 }
 
 export default TuitsController;
